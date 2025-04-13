e38b8e405166c723fe8a45b7b7afe155c4633f4c
HARSH RAI <hrai1942@gmail.com>
2025-04-13 15:42:49 +0530

    add solutions

---
+++ A2Z/Step_15_Graphs_Concepts_Problems/Lec_5_MinimumSpanningTree_Disjoint_Set_and_Problems/Disjoint_Set_Union.cpp
@@ -0,0 +1,59 @@
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+int find(int a[], int x);
+void unionSet(int a[], int x, int z);
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n + 1];
+        for (int i = 1; i <= n; i++)
+            a[i] = i;
+        int k;
+        cin >> k;
+        for (int i = 0; i < k; i++) {
+            string s;
+            cin >> s;
+            if (s == "UNION") {
+                int z, x;
+                cin >> x >> z;
+                unionSet(a, x, z);
+            } else {
+                int x;
+                cin >> x;
+                int parent = find(a, x);
+                cout << parent << " ";
+            }
+        }
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the functions below*/
+int find(int par[], int x) {
+    // add code here
+    if(par[x]==x){
+        return x;
+    }
+    return par[x]=find(par,par[x]);
+}
+
+void unionSet(int par[], int x, int z) {
+    // add code here.
+    int px = find(par, x);
+    int pz = find(par, z);
+    if (px != pz) {
+        par[px] = pz; // make parent of z's become parent of x's parent
+    } 
+}
--- A2Z/Step_15_Graphs_Concepts_Problems/Lec_5_MinimumSpanningTree_Disjoint_Set_and_Problems/Number_of_operations_to_make_network_connected.cpp
+++ A2Z/Step_15_Graphs_Concepts_Problems/Lec_5_MinimumSpanningTree_Disjoint_Set_and_Problems/Number_of_operations_to_make_network_connected.cpp
@@ -1,1 +1,68 @@
 /* Problem: Number_of_operations_to_make_network_connected */
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    class DisjointSet {
+        vector<int> rank, parent;
+    public:
+        int extra=0,comp=-1,n;
+        DisjointSet(int n) {
+            this->n=n;
+            rank.resize(n + 1, 0);
+            parent.resize(n + 1);
+            for (int i = 0; i <= n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        int findUPar(int node) {
+            if (node == parent[node])
+                return node;
+            return parent[node] = findUPar(parent[node]);
+        }
+
+        void unionByRank(int u, int v) {
+            int ulp_u = findUPar(u);
+            int ulp_v = findUPar(v);
+            if (ulp_u == ulp_v) extra++;
+            if (rank[ulp_u] < rank[ulp_v]) {
+                parent[ulp_u] = ulp_v;
+            }
+            else if (rank[ulp_v] < rank[ulp_u]) {
+                parent[ulp_v] = ulp_u;
+            }
+            else {
+                parent[ulp_v] = ulp_u;
+                rank[ulp_u]++;
+            }
+        }
+
+        void components(){
+            for (int i = 1; i <= n; i++) {
+                if(parent[i] == i){
+                    comp++;
+                }
+            }
+        }
+    };
+
+    int makeConnected(int n, vector<vector<int>>& connections) {
+    DisjointSet ds(n);
+    for (vector<int> c: connections){
+        int a=c[0];
+        int b=c[1];
+        ds.unionByRank(a+1,b+1);
+    }
+
+    ds.components();
+    if(ds.comp<=ds.extra){
+        return ds.comp;
+    }
+    else{
+        return -1;
+    }
+
+    }
+};
