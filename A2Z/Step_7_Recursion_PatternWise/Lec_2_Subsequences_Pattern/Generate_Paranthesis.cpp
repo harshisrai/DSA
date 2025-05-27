92f61d786126289b683b4f586854050fe42fb5d5
HARSH RAI <hrai1942@gmail.com>
2025-05-27 15:19:27 +0530

    solve more

--- A2Z/Step_7_Recursion_PatternWise/Lec_2_Subsequences_Pattern/Generate_Paranthesis.cpp
+++ A2Z/Step_7_Recursion_PatternWise/Lec_2_Subsequences_Pattern/Generate_Paranthesis.cpp
@@ -1,1 +1,26 @@
 /* Problem: Generate_Paranthesis */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans; 
+        string s; 
+        solve(0,0,n,ans,s);
+        return ans;
+    }
+    void solve(int open, int close, int n,vector<string>& ans, string s){
+        if(open>close){
+            s.push_back(')'); 
+            solve(open, close+1, n,ans,s);
+            s.pop_back();
+        }
+        if(open<n){
+        s.push_back('(');
+        solve(open+1, close, n,ans,s);
+        }
+        if(close==n) {ans.push_back(s); return;}
+    }
+};
--- A2Z/Step_7_Recursion_PatternWise/Lec_2_Subsequences_Pattern/Letter_Combinations_of_a_Phone_number.cpp
+++ A2Z/Step_7_Recursion_PatternWise/Lec_2_Subsequences_Pattern/Letter_Combinations_of_a_Phone_number.cpp
@@ -1,1 +1,25 @@
 /* Problem: Letter_Combinations_of_a_Phone_number */
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        vector<string> map={"abc","def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};  
+        solve(ans,map, digits, "",0);
+        return ans;
+    }
+    void solve(vector<string>& ans, vector<string>& map, string digits, string s, int idx){
+        if(idx==digits.size()){
+            if(idx!=0) ans.push_back(s);
+            return;
+        }
+        string f= map[digits[idx]-'2']; 
+        for(char ch:f){
+            s.push_back(ch); 
+            solve(ans, map, digits, s, idx+1);
+            s.pop_back();
+        }
+    }
+};
