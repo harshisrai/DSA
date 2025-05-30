23d4dbb32799bc11f7ba50985bb8298b92b4f30b
HARSH RAI <hrai1942@gmail.com>
2025-05-30 23:21:17 +0530

    solve more

--- A2Z/Step_12_Greedy_Algorithms_Easy_Medium_Hard/Lec_1_Easy_Problems/Valid_Paranthesis_Checker.cpp
+++ A2Z/Step_12_Greedy_Algorithms_Easy_Medium_Hard/Lec_1_Easy_Problems/Valid_Paranthesis_Checker.cpp
@@ -1,1 +1,24 @@
 /* Problem: Valid_Paranthesis_Checker */
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int low = 0, high = 0;
+
+        for (char ch : s) {
+            if (ch == '(') {
+                low++; high++;
+            } else if (ch == ')') {
+                low--; high--;
+            } else { // '*'
+                low--; high++;
+            }
+            if (high < 0) return false;
+            if (low < 0) low = 0;
+        }
+
+        return low == 0;
+    }
+};
