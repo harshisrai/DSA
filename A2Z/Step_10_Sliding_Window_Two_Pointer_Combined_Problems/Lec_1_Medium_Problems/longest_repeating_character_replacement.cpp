655df9d543fd508d6e77958de53f24b62f82d549
HARSH RAI <hrai1942@gmail.com>
2025-06-10 19:38:54 +0530

    solve more

--- A2Z/Step_10_Sliding_Window_Two_Pointer_Combined_Problems/Lec_1_Medium_Problems/longest_repeating_character_replacement.cpp
+++ A2Z/Step_10_Sliding_Window_Two_Pointer_Combined_Problems/Lec_1_Medium_Problems/longest_repeating_character_replacement.cpp
@@ -1,2 +1,27 @@
 /* Problem: longest_repeating_character_replacement */
 
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l=0, r=0; 
+        int maxCount=0;
+        int arr[26] = {0}; //short hand initalization ninja tehcniquw
+        while(r<s.size()){
+            arr[s[r]-'A']++;
+            maxCount=max(maxCount,arr[s[r]-'A']);
+
+            while(maxCount+k<r-l+1){
+                //the condition of loop is as such that - everty time 
+                // we only look over the largest window possible thus r-l will return the ans
+                // irrespective of whether actual length is less or more than maxCount+k
+                arr[s[l]-'A']--;
+                l++;
+            }
+            r++;
+        } 
+        // return (maxCount+k>s.size())? s.size():maxCount+k;
+        return r-l;
+
+    }
+};
--- A2Z/Step_10_Sliding_Window_Two_Pointer_Combined_Problems/Lec_2_Hard_Problems/Longest_Substring_with_At_Most_K_Distinct_Characters.cpp
+++ A2Z/Step_10_Sliding_Window_Two_Pointer_Combined_Problems/Lec_2_Hard_Problems/Longest_Substring_with_At_Most_K_Distinct_Characters.cpp
@@ -1,1 +1,51 @@
 /* Problem: Longest_Substring_with_At_Most_K_Distinct_Characters */
+
+// User function template for C++
+
+class Solution {
+  public:
+    int longestKSubstr(string &s, int k) {
+        // your code here
+        int l=0,r=0,cnt=0,maxLen=-1; 
+        int arr[26]={0}; 
+        
+        while(r<s.size()){
+            if(arr[s[r]-'a']==0) cnt++;
+            arr[s[r]-'a']++;
+            
+            if(cnt>k){
+                if(arr[s[l]-'a']==1) cnt--;
+                arr[s[l]-'a']--;
+                l++;
+            }
+            
+            /*
+            if(cnt>k){
+                if(arr[s[l]-'a']==1) cnt--;
+                arr[s[l]-'a']--;
+                l++;
+            }
+            
+            could even be 
+            
+            while(cnt>k){
+                if(arr[s[l]-'a']==1) cnt--;
+                arr[s[l]-'a']--;
+                l++;
+            }
+            
+            but the draw back was that you were decresing the window size aggresively 
+            to maintain a valid one. 
+            
+            The point is that why should we look for windows smaller than the max which
+            we have already seen. The if approach ensures that we only look for valid
+            windows which are equal or greater than the window we have already seen.
+            
+            */
+            
+            if(cnt==k) maxLen=max(maxLen, r-l+1); 
+            r++;
+        }
+        return maxLen;
+    }
+};
