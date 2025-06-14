197f0d8d2ed8d1fafbbad6cc81092cedb7b34cb7
HARSH RAI <hrai1942@gmail.com>
2025-06-15 00:19:41 +0530

    solve more

--- A2Z/Step_9_Stack_and_Queues_Learning_Pre_In_Post_fix_Monotonic/Lec_3_Monotonic_Stack_Queue_Problems_VVV_Imp/Number_of_NGEs_to_the_right.cpp
+++ A2Z/Step_9_Stack_and_Queues_Learning_Pre_In_Post_fix_Monotonic/Lec_3_Monotonic_Stack_Queue_Problems_VVV_Imp/Number_of_NGEs_to_the_right.cpp
@@ -1,1 +1,18 @@
 /* Problem: Number_of_NGEs_to_the_right */
+
+// User function Template for C++
+
+class Solution {
+  public:
+
+    vector<int> count_NGE(int n, vector<int> &arr, int queries, vector<int> &indices) {
+        // write your code here
+        vector<int> ans; 
+        for(i: indices){
+            int k=arr[i],l=0;
+            for(int j=i+1;j<n; j++) if(arr[j]>k) l++;
+            ans.push_back(l); 
+        }
+        return ans; 
+    }
+};
