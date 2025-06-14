7985e86bd0f3169a0bd09af357f900d4976a27ff
HARSH RAI <hrai1942@gmail.com>
2025-06-15 00:04:20 +0530

    solve more

--- A2Z/Step_9_Stack_and_Queues_Learning_Pre_In_Post_fix_Monotonic/Lec_3_Monotonic_Stack_Queue_Problems_VVV_Imp/Next_Greater_Element_2.cpp
+++ A2Z/Step_9_Stack_and_Queues_Learning_Pre_In_Post_fix_Monotonic/Lec_3_Monotonic_Stack_Queue_Problems_VVV_Imp/Next_Greater_Element_2.cpp
@@ -1,1 +1,19 @@
 /* Problem: Next_Greater_Element_2 */
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int k=nums.size(); 
+        vector<int> ans(k,0); 
+        stack<int> st; 
+        int n=2*k-1; 
+        for(;n>=0; n--){
+            int idx=n%k; 
+            while(!st.empty() && st.top()<=nums[idx]) st.pop();
+            ans[idx]= st.empty()? -1: st.top();  
+            // cout<<idx<<ans[idx]<<endl;
+            st.push(nums[idx]); 
+        }
+        return ans; 
+    }
+};
