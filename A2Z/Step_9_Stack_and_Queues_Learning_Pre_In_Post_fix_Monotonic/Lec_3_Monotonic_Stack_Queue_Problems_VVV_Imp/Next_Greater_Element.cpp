e86b3ad3eface69ed2bf995501ca7408fe788a81
HARSH RAI <hrai1942@gmail.com>
2025-06-14 23:45:04 +0530

    solve more

--- A2Z/Step_9_Stack_and_Queues_Learning_Pre_In_Post_fix_Monotonic/Lec_3_Monotonic_Stack_Queue_Problems_VVV_Imp/Next_Greater_Element.cpp
+++ A2Z/Step_9_Stack_and_Queues_Learning_Pre_In_Post_fix_Monotonic/Lec_3_Monotonic_Stack_Queue_Problems_VVV_Imp/Next_Greater_Element.cpp
@@ -1,1 +1,27 @@
 /* Problem: Next_Greater_Element */
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> ans;
+    unordered_map<int,int> mp; 
+    stack<int> st; 
+    for(int i=nums2.size()-1; i>=0; i--){
+        if(st.empty()) mp[nums2[i]]=-1, st.push(nums2[i]); 
+        else if(st.top()<nums2[i]){
+            while(!st.empty() && st.top() <= nums2[i]) st.pop(); 
+            mp[nums2[i]]=st.empty() ? -1 : st.top();
+            st.push(nums2[i]); 
+        }
+        else{
+            mp[nums2[i]]=st.top(); 
+            st.push(nums2[i]);
+        }
+    }
+
+    for(int i:nums1){
+        ans.push_back(mp[i]); 
+    }  
+    return ans;  
+    }
+};
