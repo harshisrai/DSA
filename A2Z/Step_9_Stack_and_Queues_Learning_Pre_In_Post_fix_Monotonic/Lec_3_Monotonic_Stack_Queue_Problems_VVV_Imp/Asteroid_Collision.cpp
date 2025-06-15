993c61971b949b7eab3a0a0a288cfbc5fea58481
HARSH RAI <hrai1942@gmail.com>
2025-06-15 18:18:59 +0530

    solve more

--- A2Z/Step_9_Stack_and_Queues_Learning_Pre_In_Post_fix_Monotonic/Lec_3_Monotonic_Stack_Queue_Problems_VVV_Imp/Asteroid_Collision.cpp
+++ A2Z/Step_9_Stack_and_Queues_Learning_Pre_In_Post_fix_Monotonic/Lec_3_Monotonic_Stack_Queue_Problems_VVV_Imp/Asteroid_Collision.cpp
@@ -1,1 +1,22 @@
 /* Problem: Asteroid_Collision */
+
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> st; 
+        for(int i:asteroids){
+            // if top is +ve, and curr is -ve
+            // pop till less or equal magnitude are out - if empty, -ve push else not 
+            if(i<0){
+                i=-i; 
+                while(!st.empty() && st.back()<i && st.back()>0) st.pop_back(); 
+                if(st.empty() || st.back()<0) st.push_back(-i); 
+                else if(st.back()==i) st.pop_back(); 
+            }
+            else{
+                st.push_back(i); 
+            }
+        }
+        return st; 
+    }
+};
