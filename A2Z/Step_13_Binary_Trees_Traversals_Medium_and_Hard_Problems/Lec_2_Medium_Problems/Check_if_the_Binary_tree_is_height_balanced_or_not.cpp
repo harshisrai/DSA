16484e4385f479113dc79365cb5eab53ea2af258
HARSH RAI <hrai1942@gmail.com>
2025-07-10 23:10:23 +0530

    solve more

--- A2Z/Step_13_Binary_Trees_Traversals_Medium_and_Hard_Problems/Lec_2_Medium_Problems/Check_if_the_Binary_tree_is_height_balanced_or_not.cpp
+++ A2Z/Step_13_Binary_Trees_Traversals_Medium_and_Hard_Problems/Lec_2_Medium_Problems/Check_if_the_Binary_tree_is_height_balanced_or_not.cpp
@@ -1,1 +1,31 @@
 /* Problem: Check_if_the_Binary_tree_is_height_balanced_or_not */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return balance(root)!=-1;
+    }
+    int balance(TreeNode* root){
+        if(root==NULL) return 0; 
+
+        int lh=balance(root->left); 
+        if(lh==-1) return -1; 
+        int rh=balance(root->right); 
+        if(rh==-1) return -1; 
+
+        //current node balanced or not
+        if(abs(lh-rh)>1) return -1;
+        return max(lh,rh)+1;
+    }
+};
--- A2Z/Step_13_Binary_Trees_Traversals_Medium_and_Hard_Problems/Lec_2_Medium_Problems/Maximum_path_sum.cpp
+++ A2Z/Step_13_Binary_Trees_Traversals_Medium_and_Hard_Problems/Lec_2_Medium_Problems/Maximum_path_sum.cpp
@@ -1,1 +1,28 @@
 /* Problem: Maximum_path_sum */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        int m=-1100;
+        sum(root,m); 
+        return m;
+    }
+    int sum(TreeNode* root, int &m){
+        if(root==NULL) return 0; 
+        int ls=max(0,sum(root->left,m)); 
+        int rs=max(0,sum(root->right,m));
+        m=max(m, ls+rs+root->val);
+        return root->val+max(ls,rs); 
+    }
+};
