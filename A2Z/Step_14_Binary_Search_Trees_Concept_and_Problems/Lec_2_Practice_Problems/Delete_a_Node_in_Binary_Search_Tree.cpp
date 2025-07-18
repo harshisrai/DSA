1e73d95087f0633394ee6daa28e1afbd89c67520
HARSH RAI <hrai1942@gmail.com>
2025-07-19 00:16:33 +0530

    solve more

--- A2Z/Step_14_Binary_Search_Trees_Concept_and_Problems/Lec_2_Practice_Problems/Delete_a_Node_in_Binary_Search_Tree.cpp
+++ A2Z/Step_14_Binary_Search_Trees_Concept_and_Problems/Lec_2_Practice_Problems/Delete_a_Node_in_Binary_Search_Tree.cpp
@@ -1,1 +1,42 @@
 /* Problem: Delete_a_Node_in_Binary_Search_Tree */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(!root) return nullptr; 
+        if(root->val<key) root->right=deleteNode(root->right, key); 
+        else if(root->val>key)  root->left=deleteNode(root->left, key); 
+        else{
+            // if matches
+            if(!root->left && !root->right){delete root; return nullptr;} //leaf
+            if(!root->left){ //only right child
+                TreeNode* temp=root->right; 
+                delete root; 
+                return temp; 
+            }
+            if(!root->right){ //only left child
+                TreeNode* temp=root->left; 
+                delete root; 
+                return temp; 
+            }
+            
+            //if both child - either right successor (leftmost in right subtree) or left predecessor (rightmost in left subtree)
+            TreeNode* succ=root->right; //successor logic used here
+            while(succ->left) succ=succ->left; 
+            root->val=succ->val; 
+            root->right= deleteNode(root->right, succ->val); // Delete successor
+        }
+        return root; 
+    }
+};
