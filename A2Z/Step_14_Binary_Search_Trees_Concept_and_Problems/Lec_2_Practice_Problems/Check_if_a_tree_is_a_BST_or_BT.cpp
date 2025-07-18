81c3cad311e023d0a256139f281b5eb92bdd096e
HARSH RAI <hrai1942@gmail.com>
2025-07-19 00:53:56 +0530

    solve more

--- A2Z/Step_14_Binary_Search_Trees_Concept_and_Problems/Lec_2_Practice_Problems/Check_if_a_tree_is_a_BST_or_BT.cpp
+++ A2Z/Step_14_Binary_Search_Trees_Concept_and_Problems/Lec_2_Practice_Problems/Check_if_a_tree_is_a_BST_or_BT.cpp
@@ -1,1 +1,25 @@
 /* Problem: Check_if_a_tree_is_a_BST_or_BT */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    bool validate(TreeNode* Node, long max, long min){
+        if(!Node) return true;
+        if(Node->val>=max || Node->val<=min) return false; 
+        return validate(Node->left, Node->val, min) && validate(Node->right,max,Node->val);
+    }
+public:
+    bool isValidBST(TreeNode* root) {
+        return validate(root, LONG_MAX, LONG_MIN); 
+    }
+};
--- A2Z/Step_14_Binary_Search_Trees_Concept_and_Problems/Lec_2_Practice_Problems/LCA_in_Binary_Search_Tree.cpp
+++ A2Z/Step_14_Binary_Search_Trees_Concept_and_Problems/Lec_2_Practice_Problems/LCA_in_Binary_Search_Tree.cpp
@@ -1,1 +1,11 @@
 /* Problem: LCA_in_Binary_Search_Tree */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root) return nullptr; 
+        if(root->val>p->val && root->val>q->val) return lowestCommonAncestor(root->left, p,q); 
+        if(root->val<p->val && root->val<q->val) return lowestCommonAncestor(root->right, p,q); 
+        return root;  //value lies in between or matches either p or q
+    }
+};
