4946420b28d16eddb2d11a6036caa66256b5ddfc
HARSH RAI <hrai1942@gmail.com>
2025-07-19 17:17:54 +0530

    solve more

--- A2Z/Step_15_Graphs_Concepts_Problems/Lec_2_Problems_on_BFS_DFS/Bipartite_Graph_DFS.cpp
+++ A2Z/Step_15_Graphs_Concepts_Problems/Lec_2_Problems_on_BFS_DFS/Bipartite_Graph_DFS.cpp
@@ -1,1 +1,26 @@
 /* Problem: Bipartite_Graph_DFS */
+
+class Solution {
+public:
+    bool dfs(int node, int c,vector<vector<int>>& graph, vector<int>& color){
+        color[node]=c;
+        for(auto it:graph[node]){
+            if(color[it]==-1){
+                if(!dfs(it, !c,graph,color)) return false;
+            }
+            else{
+                if(color[it]==c)return false;
+            }
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int v=graph.size();
+        vector<int> color(v,-1);
+        int c=0;
+        for(int i=0;i<v;i++){
+           if(color[i]==-1) if(!dfs(i,c,graph,color)) return false;
+        }
+        return true;
+    }
+};
--- A2Z/Step_15_Graphs_Concepts_Problems/Lec_2_Problems_on_BFS_DFS/Number_of_Enclaves_flood_fill_implementation_multisource.cpp
+++ A2Z/Step_15_Graphs_Concepts_Problems/Lec_2_Problems_on_BFS_DFS/Number_of_Enclaves_flood_fill_implementation_multisource.cpp
@@ -1,1 +1,39 @@
 /* Problem: Number_of_Enclaves_flood_fill_implementation_multisource */
+
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int m=grid.size(); 
+        int n=grid[0].size();
+        queue<pair<int,int>> q; 
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0 || i==m-1 || j==n-1){
+                    if(grid[i][j]==1) q.push({i,j}),grid[i][j]=0;;
+                }
+            }
+        }
+
+        int dx[4]={0,0,-1,1};
+        int dy[4]={1,-1,0,0};
+        while(!q.empty()){
+            auto[x,y]=q.front();
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nx=x+dx[i], ny=y+dy[i];
+                if(nx>=0 && ny >=0 && nx<m && ny<n){
+                    if(grid[nx][ny]==1) q.push({nx,ny}), grid[nx][ny]=0;;
+                }
+            }
+        }
+
+        int ans=0;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1) ans++;
+            }
+        }
+        return ans;
+    }
+};
