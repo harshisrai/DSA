47a920043463112e7804b44c5ae5a689c05ab793
HARSH RAI <hrai1942@gmail.com>
2025-07-19 20:46:38 +0530

    solve more

--- A2Z/Step_15_Graphs_Concepts_Problems/Lec_2_Problems_on_BFS_DFS/0_1_Matrix_Bfs_Problem.cpp
+++ A2Z/Step_15_Graphs_Concepts_Problems/Lec_2_Problems_on_BFS_DFS/0_1_Matrix_Bfs_Problem.cpp
@@ -1,1 +1,35 @@
 /* Problem: 0_1_Matrix_Bfs_Problem */
+
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m=mat.size();
+        int n=mat[0].size();
+        vector<vector<int>> vis(m,vector<int>(n,-1));
+        queue<pair<int,int>> q; 
+        // bfs - gives you shorted distance from where you start
+        // so if we start from what needs to be nearest (here zero), we can assign the shortest value possible
+
+        // it seems perfectly sensible to start from all the zeroes in the matrix so that we can give
+        // the shortest distance to all the ones in the matrix from the zeroes
+        // thus we will do multi-source BFS
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0) q.push({i,j}),vis[i][j]=0;
+            }
+        
+        int dx[4]={0,0,1,-1};
+        int dy[4]={1,-1,0,0};
+
+        while(!q.empty()){
+            auto[x,y]=q.front();q.pop();
+            for(int i=0;i<4;i++){
+                int nx=x+dx[i], ny=y+dy[i];
+                if(nx>=0 && ny>=0 && nx<m && ny<n){
+                    if(vis[nx][ny]==-1) vis[nx][ny]=1+vis[x][y], q.push({nx,ny});
+                }
+            }
+        }
+        return vis;
+    }
+};
--- A2Z/Step_15_Graphs_Concepts_Problems/Lec_4_Shortest_Path_Algorithms_and_Problems/Why_priority_Queue_is_used_in_Djisktra_s_Algorithm.cpp
+++ A2Z/Step_15_Graphs_Concepts_Problems/Lec_4_Shortest_Path_Algorithms_and_Problems/Why_priority_Queue_is_used_in_Djisktra_s_Algorithm.cpp
@@ -1,1 +1,39 @@
 /* Problem: Why_priority_Queue_is_used_in_Djisktra_s_Algorithm */
+
+
+class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        // Code here
+        vector<vector<pair<int,int>>> adj(V);
+        for(auto& edge:edges){
+            int u=edge[0],v=edge[1],w=edge[2];
+            adj[u].push_back({v,w});
+            adj[v].push_back({u,w});
+        }
+        
+        
+        vector<int> dist(V,INT_MAX);
+        priority_queue<pair<int,int>, vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        dist[src]=0;
+        pq.push({0,src});
+        while(!pq.empty()){
+            int node=pq.top().second; 
+            int dis=pq.top().first;
+            // the below line is not compulsory, code will run without it too
+            // just my obersvation that sometimes the same node gets pushed multiple times
+            if (dis > dist[node]) continue; //skipping multiple entries with higher distance for the same node
+            // that goes pushed in the priority queue during updates for the same node
+            pq.pop(); // if something is poped, it's final distance is calculated from src
+            for(auto& edge: adj[node]){
+                int v=edge.first,w=edge.second;
+                if(dis+w<dist[v]){
+                    dist[v]=dis+w;
+                    pq.push({dist[v],v});
+                }
+            }
+            
+        }
+        return dist;
+    }
+};
